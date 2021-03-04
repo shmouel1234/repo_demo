7f5104a693574aabd07b06c337f70a1345b89ace
shmouel1234 <shmouelgithub1@gmail.com>
2021-03-04 20:23:08 +0200

    first commit 20:22

---
+++ myPython1.py
@@ -0,0 +1,10 @@
+import random
+import os
+
+def cls(): return os.system('cls')
+
+cls()
+
+print("welcome to our first github project")
+name = input("what is your name? ")
+print(f"welcome to you {name}")
